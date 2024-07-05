65a25dbcd029f41cb01ae6ff959ec13b58132916
Abel Rivas Bautista <abel.rivas@Imperial.local>
2024-07-04 23:54:41 -0400

    corrección V1.0.1 (sin validaciones)

--- src/server/main.js
+++ src/server/main.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import cors from 'cors'
-import { getPost, createPost } from './models/likeme.models.js'
+import { getPost, createPost, updateLike, deletePost } from './models/likeme.models.js'
+
 const app = express()
 const PORT = process.env.PORT ?? 3000
+
 app.use(cors())
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-//   res.status(200).send('Conneción con db Post')
-// })
+app.get('/', (_, res) => res.status(200).send('Conneción con db Post'))
 
-app.get('/posts', async (req, res) => {
+app.get('/posts', async (_, res) => {
   try {
     const posts = await getPost()
     res.status(200).send(posts)
@@ -21,12 +21,34 @@ app.get('/posts', async (req, res) => {
 
 app.post('/posts', async (req, res) => {
   try {
-    await createPost(req.body)
-    console.log(req.body)
+    const { titulo, url, descripcion } = req.body
+    await createPost(titulo, url, descripcion)
     res.status(201).send('Post creado con exito')
   } catch (error) {
     res.status(400).send(error)
   }
 })
+
+app.put('/posts/like/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    await updateLike(id)
+    res.status(200).send('Like actualizado')
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+app.delete('/posts/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    await deletePost(id)
+    res.status(200).send('Post eliminado')
+  } catch (error) {
+    res.status(500).send('No se encontró el dato', error)
+  }
+})
+
+app.all('*', (_, res) => res.status(404).send('Page no found'))
 
 app.listen(PORT, () => console.log(`Server inciado en el puerto ${PORT}`))
--- src/server/models/likeme.models.js
+++ src/server/models/likeme.models.js
@@ -1,6 +1,10 @@
 import db from '../database/db_connect.js'
 
-export const getPost = async () => await db('SELECT * FROM posts;')
+export const getPost = async () => await db('SELECT * FROM posts ORDER BY id ASC;')
 
-export const createPost = async (titulo, url, descripcion, likes) =>
+export const createPost = async (titulo, url, descripcion, likes = 0) =>
   await db('INSERT INTO posts (titulo,img,descripcion,likes) VALUES ($1,$2,$3,$4) RETURNING *;', [titulo, url, descripcion, likes])
+
+export const updateLike = async (id) => await db('UPDATE posts SET likes = likes +1 WHERE id = $1 RETURNING *;', [id])
+
+export const deletePost = async (id) => await db('DELETE FROM posts WHERE id = $1 RETURNING *;', [id])
